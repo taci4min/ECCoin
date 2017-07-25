eced26faedae6e16739a7b40d64c6d02b46183e3
greg-griffith <griffith@cryptounited.io>
2017-07-25 18:35:56 +0000

    updated leveldb

--- src/crypto/crypto_endian.h
+++ src/crypto/crypto_endian.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_COMPAT_ENDIAN_H
-#define BITCOIN_COMPAT_ENDIAN_H
+#ifndef _SYS_ENDIAN_H_BITCOIN
+#define _SYS_ENDIAN_H_BITCOIN
 
 #if defined(HAVE_CONFIG_H)
 #include "config/bitcoin-config.h"
@@ -13,11 +13,11 @@
 
 #include "byteswap.h"
 
-#ifndef WIN32
+#ifdef __linux__
 
 #include <endian.h>
 
-#else
+#elif
 
 #if defined(WORDS_BIGENDIAN)
 
@@ -192,5 +192,7 @@ inline uint64_t le64toh(uint64_t little_endian_64bits)
 #endif // HAVE_DECL_LE64TOH
 
 #endif // WORDS_BIGENDIAN
-#endif // winows
+
+#endif
+
 #endif // BITCOIN_COMPAT_ENDIAN_H
